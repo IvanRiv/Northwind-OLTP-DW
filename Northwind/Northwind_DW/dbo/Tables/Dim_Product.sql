afbdb96e2f26543ee7dfe144352bbe33a4523511
Ivan Sergio Rivera Vidaurre <72421966+IvanRiv@users.noreply.github.com>
2026-05-08 23:18:09 -0400

    Delete Northwind/Northwind_DW directory

--- Northwind/Northwind_DW/dbo/Tables/Dim_Product.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Dim_Product] (
-    [ProductKey]   INT           IDENTITY (1, 1) NOT NULL,
-    [ProductID]    INT           NULL,
-    [ProductName]  NVARCHAR (40) NULL,
-    [CategoryName] NVARCHAR (15) NULL,
-    PRIMARY KEY CLUSTERED ([ProductKey] ASC)
-);
-
